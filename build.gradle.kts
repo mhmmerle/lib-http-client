16f3e36f683ca52d9bc83fab36d20dea8a4f8fc4
Marc Haemmerle <marc.haemmerle@adcubum.com>
2019-12-19 17:09:41 +0100

    Update to gradle 6.0.1, also publish sources and javadoc

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,30 +1,33 @@
 plugins {
-    id("email.haemmerle.baseplugin").version("0.0.5")
+    id("email.haemmerle.baseplugin").version("1.0.3")
 }
 
 group = "email.haemmerle.restclient"
 description = "RESTful HTTP Client Library"
-
-buildScan {
-    termsOfServiceUrl = "https://gradle.com/terms-of-service"
-    termsOfServiceAgree = "yes"
-}
 
 `email-haemmerle-base`{
     username = "mhmmerle"
 }
 
 dependencies {
+    implementation(platform("org.apache.logging.log4j:log4j-bom:2.12.1"))
+    testImplementation(platform("org.junit:junit-bom:5.5.2"))
+
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
     implementation("com.github.kittinunf.fuel:fuel:2.2.1")
     implementation("com.beust:klaxon:5.0.13")
-    implementation("org.apache.logging.log4j:log4j-api:2.11.1")
-    implementation("org.apache.logging.log4j:log4j-core:2.11.1")
+    implementation("org.apache.logging.log4j:log4j-api")
+    implementation("org.apache.logging.log4j:log4j-core")
 
-    testImplementation("org.jetbrains.kotlin:kotlin-test")
-    testImplementation("org.jetbrains.kotlin:kotlin-test-junit")
-    testImplementation("org.junit.jupiter:junit-jupiter-api:5.5.2")
-    testImplementation("org.junit.jupiter:junit-jupiter-params:5.5.2")
+    testImplementation("org.junit.jupiter:junit-jupiter-api")
+    testImplementation("org.junit.jupiter:junit-jupiter-params")
     testImplementation("org.assertj:assertj-core:3.11.0")
     testImplementation("com.squareup.okhttp3:mockwebserver:4.2.0")
+
+    testRuntimeOnly("org.junit.platform:junit-platform-console")
+}
+
+java {
+    withJavadocJar()
+    withSourcesJar()
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,8 +1,20 @@
-rootProject.name = "lib-rest-client"
-
 pluginManagement{
     repositories{
         gradlePluginPortal()
         maven(url = "https://dl.bintray.com/mhmmerle/snapshots")
     }
 }
+
+plugins {
+    id("com.gradle.enterprise").version("3.1.1")
+}
+
+gradleEnterprise {
+    buildScan {
+        termsOfServiceUrl = "https://gradle.com/terms-of-service"
+        termsOfServiceAgree = "yes"
+    }
+}
+
+rootProject.name = "restclient"
+
